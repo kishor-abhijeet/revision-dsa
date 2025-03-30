9feccedd9b9fa4ee8eeab76255db624d044ae245
abhijeet kishor <abhijeetkishor24@gmail.com>
2025-03-30 12:30:03 +0530

    Create string rotation of each other.cpp

---
+++ STRING/string rotation of each other.cpp
@@ -0,0 +1,19 @@
+//Approach : 1
+bool rotateString(string &s, string &goal) {
+       
+        
+        int len=s.length();
+        for(int i=0;i<len;i++)
+        {
+            s+=s[i];
+            string sub=s.substr(i+1,len+1);
+            if(sub==goal)
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+
+//Approach :- 2()
