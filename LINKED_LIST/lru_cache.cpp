e4a6d07378deb42a86f18b9b15ca31e1cf0d2578
Abhijeet Kishor <abhijeetkishor24@gmail.com>
2025-05-31 23:03:53 +0530

    Update lru_cache.cpp

--- LINKED_LIST/lru_cache.cpp
+++ LINKED_LIST/lru_cache.cpp
@@ -1,71 +1,101 @@
-// C++ program to implement LRU Least Recently Used) using
-//Built-in Doubly linked list
+// C++ program to implement LRU Least Recently Used)
 #include <bits/stdc++.h>
 using namespace std;
 
-class LRUCache {
+struct Node {
+    int key;
+    int value;
+    Node *next;
+    Node *prev;
+
+    Node(int k, int v) {
+        key = k;
+        value = v;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+// LRU Cache class
+class LRUCache
+{
   public:
+  
+    // Constructor to initialize the cache with a given capacity
     int capacity;
-    list<pair<int, int>> List;
-
-    // Map from key to list iterator
-    unordered_map<int, list<pair<int, int>>::iterator> cacheMap;
-
-    // Constructor to initialize the 
-  	//cache with a given capacity
+    unordered_map<int, Node *> cacheMap;
+    Node *head;
+    Node *tail;
     LRUCache(int capacity) {
         this->capacity = capacity;
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head->next = tail;
+        tail->prev = head;
     }
 
     // Function to get the value for a given key
     int get(int key) {
-        auto it = cacheMap.find(key);
-        if (it == cacheMap.end()) {
+      
+        if (cacheMap.find(key) == cacheMap.end())
             return -1;
-        }
+  
 
-        // Move the accessed node to the 
-      	//front (most recently used position)
-        int value = it->second->second;
-        List.erase(it->second);
-        List.push_front({key, value});
-
-        // Update the iterator in the map
-        cacheMap[key] = List.begin();
-        return value;
+        Node *node = cacheMap[key];
+        remove(node);
+        add(node);
+        return node->value;
     }
 
     // Function to put a key-value pair into the cache
     void put(int key, int value) {
-        auto it = cacheMap.find(key);
-        if (it != cacheMap.end()) {
-            // Remove the old node from the list and map
-            List.erase(it->second);
-            cacheMap.erase(it);
+        if (cacheMap.find(key) != cacheMap.end()) {
+            Node *oldNode = cacheMap[key];
+            remove(oldNode);
+          	delete oldNode;
+          
         }
 
-        // Insert the new node at the front of the list
-        List.push_front({key, value});
-        cacheMap[key] = List.begin();
-
-        // If the cache size exceeds the capacity,
-      	//remove the least recently used item
+        Node *node = new Node(key, value);
+        cacheMap[key] = node;
+        add(node);
+       
+       
         if (cacheMap.size() > capacity) {
-            auto lastNode = List.back().first;
-            List.pop_back();
-            cacheMap.erase(lastNode);
+            Node *nodeToDelete = tail->prev;
+            remove(nodeToDelete);
+            cacheMap.erase(nodeToDelete->key);
+          	delete nodeToDelete;
         }
+    }
+
+    // Add a node right after the head 
+  	// (most recently used position)
+    void add(Node *node) {
+        Node *nextNode = head->next;
+        head->next = node;
+        node->prev = head;
+        node->next = nextNode;
+        nextNode->prev = node;
+    }
+
+    // Remove a node from the list
+    void remove(Node *node) {
+        Node *prevNode = node->prev;
+        Node *nextNode = node->next;
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
     }
 };
 
-int main() {
-  
+int main(){
     LRUCache cache(2);
-    cache.put(1, 1);
+  
+    cache.put(1, 1); 
     cache.put(2, 2);
     cout << cache.get(1) << endl;
     cache.put(3, 3);
-    cout << cache.get(2) << endl;
+    cout  << cache.get(2) << endl;
     cache.put(4, 4);
     cout << cache.get(1) << endl;
     cout << cache.get(3) << endl;
