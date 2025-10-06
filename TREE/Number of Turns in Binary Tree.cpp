7ecc0775e2b7a2e1d53d9968c414b26063f29670
Abhijeet Kishor <abhijeetkishor24@gmail.com>
2025-10-06 21:43:38 +0530

    Update Number of Turns in Binary Tree.cpp

--- TREE/Number of Turns in Binary Tree.cpp
+++ TREE/Number of Turns in Binary Tree.cpp
@@ -34,4 +34,86 @@ APProach:-
 
 // // if key is not found.
 // return false;
- 
+
+  // function count number of turn need to reach
+// given node from it's LCA we have two way to
+bool CountTurn(Node* root, int key, bool turn,
+                                   int* count)
+{
+    if (root == NULL)
+        return false;
+
+    // if found the key value in tree
+    if (root->key == key)
+        return true;
+
+    // Case 1:
+    if (turn == true) {
+        if (CountTurn(root->left, key, turn, count))
+            return true;
+        if (CountTurn(root->right, key, !turn, count)) {
+            *count += 1;
+            return true;
+        }
+    } 
+    else // Case 2:
+    {
+        if (CountTurn(root->right, key, turn, count))
+            return true;
+        if (CountTurn(root->left, key, !turn, count)) {
+            *count += 1;
+            return true;
+        }
+    }
+    return false;
+}
+ // Function to find nodes common to given two nodes
+int NumberOFTurn(struct Node* root, int first, 
+                                   int second)
+{
+    struct Node* LCA = findLCA(root, first, second);
+
+    // there is no path between these two node
+    if (LCA == NULL)
+        return -1;
+    int Count = 0;
+
+    // case 1:
+    if (LCA->key != first && LCA->key != second) {
+        
+        // count number of turns needs to reached
+        // the second node from LCA
+        if (CountTurn(LCA->right, second, false,
+                                           &Count)
+            || CountTurn(LCA->left, second, true, 
+                                           &Count))
+            ;
+        
+        // count number of turns needs to reached 
+        // the first node from LCA
+        if (CountTurn(LCA->left, first, true, 
+                                       &Count)
+            || CountTurn(LCA->right, first, false, 
+                                       &Count))
+            ;
+        return Count + 1;
+    }
+
+    // case 2:
+    if (LCA->key == first) {
+
+        // count number of turns needs to reached 
+        // the second node from LCA
+        CountTurn(LCA->right, second, false, &Count);
+        CountTurn(LCA->left, second, true, &Count);
+        return Count;
+    } else {
+
+        // count number of turns needs to reached
+        // the first node from LCA1
+        CountTurn(LCA->right, first, false, &Count);
+        CountTurn(LCA->left, first, true, &Count);
+        return Count;
+    }
+}
+
