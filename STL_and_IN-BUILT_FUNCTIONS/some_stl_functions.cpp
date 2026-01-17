6bc4c958966dcdfaddebc28511a4df4e884a3282
Abhijeet Kishor <abhijeetkishor24@gmail.com>
2026-01-17 10:33:07 +0530

    Update some_stl_functions.cpp

--- STL_and_IN-BUILT_FUNCTIONS/some_stl_functions.cpp
+++ STL_and_IN-BUILT_FUNCTIONS/some_stl_functions.cpp
@@ -4,8 +4,27 @@ if (isalnum(ch1)) {
     } else {
         std::cout << ch1 << " is not alphanumeric." << std::endl;
     }
+
+
 //2.)// C++ program to convert whole string to
 // using transform() function and ::toupper in STL// similarly for tolower
     transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
     cout<<s1<<endl;
+
+
 //3.)isalnum() and isalpha () helps to find alphanumeric characters from a string 
+
+
+//4.) to beak the words in token wise from a string where words are sepereted by spaces , and store it into a vector string
+        vector<string> words;
+        stringstream ss(s);
+        string word;
+        while(ss >> word){
+            words.push_back(word);
+        }
+
+        for(auto &w: words){
+            cout<<w<<" ";
+        }
+
+
